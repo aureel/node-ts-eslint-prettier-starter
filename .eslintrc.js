6efe1fc1a77e5f241602c661a7b0b78aacdb681f
Aurelien Vitse <avitse@gmail.com>
2021-04-16 18:56:04 -0400

    fix eslint + husky config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    "airbnb-base",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["airbnb-base", "prettier", "plugin:prettier/recommended"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import type { Test } from "./types";
 
 const test: Test = { name: "Hi", value: "This is my TS starter" };
+
+// eslint-disable-next-line no-console
+console.log(test);
